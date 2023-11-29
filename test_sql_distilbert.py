fef1ac568a01fb09ec7ce8b31645afb4d4b5ac9e
wilyub <patrick.yubeaton@gmail.com>
2023-11-29 15:52:58 -0500

    Accuracy Tests for Base Model Added

--- test_sql_distilbert.py
+++ test_sql_distilbert.py
@@ -1,19 +1,40 @@
 from transformers import AutoTokenizer, DistilBertForSequenceClassification
 import torch
+import numpy as np
+from sklearn.metrics import accuracy_score
+from tqdm import tqdm
 
-from DistilBertModel import DistilBertModel
-from wafamole.evasion import EvasionEngine
+from datasets import load_from_disk
 
 tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
-model = DistilBertForSequenceClassification.from_pretrained("./sql_llm/sql_distilbert/")
+model = DistilBertForSequenceClassification.from_pretrained("distilbert-base-uncased")
+model_sql = DistilBertForSequenceClassification.from_pretrained("./sql_llm/sql_distilbert/")
+sql_dataset = load_from_disk("./sql_ds.hf/")
+test_set = sql_dataset['test']
+ground_truth = np.zeros(test_set.num_rows)
+predicted_labels_model = np.zeros(test_set.num_rows)
+predicted_labels_sql_model = np.zeros(test_set.num_rows)
+for i, entry in enumerate(tqdm(test_set, total=test_set.num_rows)):
+    inputs = tokenizer(entry['Query'], return_tensors="pt")
+    ground_truth[i] = entry['label']
+    with torch.no_grad():
+        logits = model(**inputs).logits
+        sql_logits = model_sql(**inputs).logits
+    predicted_class_id = logits.argmax().item()
+    predicted_labels_model[i] = predicted_class_id
+    predicted_class_id_sql = sql_logits.argmax().item()
+    predicted_labels_sql_model[i] = predicted_class_id_sql
 
-inputs = tokenizer("""
-   SELECT * FROM building WHERE saw IN  ( SELECT general FROM lift )
-    """, return_tensors="pt")
+print("Distilbert Accuracy: " + str(accuracy_score(ground_truth, predicted_labels_model)))
+print("SQL Distilbert Accuracy: " + str(accuracy_score(ground_truth, predicted_labels_sql_model)))
 
-with torch.no_grad():
+# inputs = tokenizer("""
+#    SELECT * FROM building WHERE saw IN  ( SELECT general FROM lift )
+#     """, return_tensors="pt")
 
-    logits = model(**inputs).logits
-predicted_class_id = logits.argmax().item()
-print(predicted_class_id)
+# with torch.no_grad():
+
+#     logits = model(**inputs).logits
+# predicted_class_id = logits.argmax().item()
+# print(predicted_class_id)
 
